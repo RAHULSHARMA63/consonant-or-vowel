f3013cc23dc406e89152b7fe51515b8d5910895d
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-04 13:23:04 +0530

    To check that the entered character is consonant or vowel.

---
+++ consonant or vowel.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter any character from  A to Z := ";
+    cin>>ch;
+    switch(ch)
+   {
+      case 'A':
+           cout<<"vowel";
+           break;
+          
+      case 'E':
+           cout<<"vowel";
+           break;
+     
+      case 'I':
+           cout<<"vowel";
+           break;
+     
+      case 'O':
+           cout<<"vowel";
+           break;
+     
+      case 'U':
+           cout<<"vowel";
+           break;
+                
+      case 'a':
+           cout<<"vowel";
+           break;
+          
+      case 'e':
+           cout<<"vowel";
+           break;
+     
+      case 'i':
+           cout<<"vowel";
+           break;
+     
+      case 'o':
+           cout<<"vowel";
+           break;
+     
+      case 'u':
+           cout<<"vowel";
+           break;                                        
+            
+      default:
+           cout<<"consonant";
+    }     
+	return 0;
+	
+}
